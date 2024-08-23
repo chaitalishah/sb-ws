8b9ac84a77d5836c830a5fbba9b4a4d37185ab77
Chaitali Shah <chaitali.shah09@gmail.com>
2024-08-23 14:14:13 -0400

    Update CustomerController.java

--- sb-ws/src/main/java/com/sb/controller/CustomerController.java
+++ sb-ws/src/main/java/com/sb/controller/CustomerController.java
@@ -18,6 +18,7 @@ import com.sb.repository.CustomerRepository;
 //TODO: ADD RequestParam too. So we can identify URL change in code with ? and PathVariable.
 //TODO: Add UPSERT operation via PUT
 //TODO : Add PAtch operation for only specified update 
+//TODO : Add Circuit Breaker and resilence4j code.
 
 @RestController
 @RequestMapping("/customers")
